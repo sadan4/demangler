da3cff2da9ecc4d0c52244eb170bada01dfbf921
sadan <117494111+sadan4@users.noreply.github.com>
2025-08-19 18:37:56 -0400

    update

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 set -ex
 rm -rf dist
 # esbuild --bundle src/js/index.ts --external:fs --platform=node --format=esm --outfile=dist/js/index.mjs
-esbuild --bundle src/js/index.ts --external:fs --platform=node --format=cjs --outfile=dist/js/index.js
-esbuild --bundle src/wasm/index.ts --external:fs --platform=node --format=cjs --outfile=dist/wasm/index.js
+esbuild --bundle src/js/index.ts --external:fs --platform=node --format=cjs --outfile=js/index.js
+esbuild --bundle src/wasm/index.ts --external:fs --platform=node --format=cjs --outfile=wasm/index.js
 # cp dist/js/index.js dist/js/index.cjs
-cp src/wasm/compiled.wasm dist/wasm/compiled.wasm
-cp src/wasm/compiled.wasm.map dist/wasm/compiled.wasm.map
+cp src/wasm/compiled.wasm wasm/compiled.wasm
+cp src/wasm/compiled.wasm.map wasm/compiled.wasm.map
 
 # copy type definitions
-cp ./index.d.ts dist/wasm/index.d.ts
-cp ./index.d.ts dist/js/index.d.ts
+cp ./index.d.ts wasm/index.d.ts
+cp ./index.d.ts js/index.d.ts
